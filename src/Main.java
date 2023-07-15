ef58814bd108d90c9b0ca61fe24ac4aa2571b18e
ZhannaSap <nekotyan474@gmail.com>
2023-07-16 00:42:50 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+    Boss villain = new Boss();
+    villain.setDamage(50);
+    villain.setHealth(700);
+    villain.setProtection("Magic shield");
+
+        System.out.println("Boss : "  + " Health: " + villain.getHealth() + " Damage: " + " Protection: " + villain.getProtection());
+    }
+
+    public static String createHeros (){
+        
+        return;
+    }
+}
