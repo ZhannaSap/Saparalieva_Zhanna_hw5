c0b42f59b4102545880ea6639df7b74da58dcf2e
ZhannaSap <nekotyan474@gmail.com>
2023-07-16 01:37:02 +0600

    with +hw

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,25 @@ public class Main {
     villain.setHealth(700);
     villain.setProtection("Magic shield");
 
-        System.out.println("Boss : "  + " Health: " + villain.getHealth() + " Damage: " + " Protection: " + villain.getProtection());
+        System.out.println("Boss : " );
+        System.out.println(" Health: " + villain.getHealth() + " Damage: " + " Protection: " + villain.getProtection());
+
+
+        Hero[] heroes = createHeroes();
+        System.out.print("Information about heroes: ");
+        for (Hero hero: heroes) {
+            System.out.println("Health; " + hero.getHealth());
+            System.out.println("Damage: " + hero.getDamage());
+            System.out.println("Super power: " + hero.getPower());
+        }
     }
 
-    public static String createHeros (){
-        
-        return;
+
+    public static Hero[] createHeroes (){
+        Hero hero1 = new Hero(100,15,"Telekinesis");
+        Hero hero2 = new Hero(150,20);
+        Hero hero3 = new Hero(200,25);
+        Hero[] heroes = {hero1, hero2, hero3};
+        return heroes;
     }
 }
